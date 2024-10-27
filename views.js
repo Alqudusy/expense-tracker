f5d3febecc413b821dd173f82c6606b2852c17ba
Alqudusy <yusufabdulqudus02@gmail.com>
2024-10-27 13:54:30 -0700

    Working on recent trips

--- views.js
+++ views.js
@@ -13,18 +13,30 @@ class UpdateCurrentTripsElements {
     }
     getRecentTrips() {
         this.savedTrips = JSON.parse(localStorage.getItem('trips'));
-        this.savedTrips.forEach(trip => {
+        const currentTripObj = new CurrentTrips();
+        const recentTrips = this.savedTrips.slice(-1);
+        recentTrips.forEach(trip => {
             for (const x in trip) {
                 switch (x) {
                     case 'from':
-                         
+                          currentTripObj.from = trip[x];
                         break;
-                
+                    case 'to':
+                          currentTripObj.to = trip[x];
+                        break;
+                    case 'dateOfDeparting':
+                          currentTripObj.date = trip[x];
+                        break;
+                    case 'ammountSpent':
+                          currentTripObj.amount = trip[x];
+                        break;
                     default:
                         break;
                 }
-            }
+            }        
+            currentTripsArr.push(currentTripObj);
         });
+        console.log((currentTripsArr));
     }
 }
 new UpdateCurrentTripsElements();
