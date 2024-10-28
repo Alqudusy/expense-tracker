38df833adae3cef5c31a20e938a73949b0c83183
Alqudusy <yusufabdulqudus02@gmail.com>
2024-10-28 06:54:17 -0700

    Finally almost done with recent trips

--- views.js
+++ views.js
@@ -6,37 +6,67 @@ class CurrentTrips {
         this.amount = amount;
     }
 }
+
 const currentTripsArr = [];
+
 class UpdateCurrentTripsElements {
     constructor() {
+        this.table = document.querySelector('.recent-trips');
         this.getRecentTrips();
     }
+
     getRecentTrips() {
         this.savedTrips = JSON.parse(localStorage.getItem('trips'));
-        const currentTripObj = new CurrentTrips();
-        const recentTrips = this.savedTrips.slice(-1);
+
+        const recentTrips = this.savedTrips.slice(-2);
+
         recentTrips.forEach(trip => {
+            const currentTripObj = new CurrentTrips();
             for (const x in trip) {
                 switch (x) {
                     case 'from':
-                          currentTripObj.from = trip[x];
+                        currentTripObj.from = trip[x];
                         break;
                     case 'to':
-                          currentTripObj.to = trip[x];
+                        currentTripObj.to = trip[x];
                         break;
                     case 'dateOfDeparting':
-                          currentTripObj.date = trip[x];
+                        currentTripObj.date = trip[x];
                         break;
                     case 'ammountSpent':
-                          currentTripObj.amount = trip[x];
+                        currentTripObj.amount = trip[x];
                         break;
                     default:
                         break;
                 }
-            }        
+            }
             currentTripsArr.push(currentTripObj);
         });
-        console.log((currentTripsArr));
+
+        currentTripsArr.forEach((trip) => {
+            const tripsTableRow = document.createElement('tr');
+
+            const fromCell = document.createElement('td');
+            fromCell.textContent = trip.from;
+            tripsTableRow.appendChild(fromCell);
+
+            const toCell = document.createElement('td');
+            toCell.textContent = trip.to;
+            tripsTableRow.appendChild(toCell);
+
+            const dateCell = document.createElement('td');
+            dateCell.textContent = trip.date;
+            tripsTableRow.appendChild(dateCell);
+
+            const amountCell = document.createElement('td');
+            amountCell.textContent = trip.amount;
+            tripsTableRow.appendChild(amountCell);
+
+            this.table.appendChild(tripsTableRow);
+        });
+
+        console.log(currentTripsArr);
     }
 }
+
 new UpdateCurrentTripsElements();
